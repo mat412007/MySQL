7b42af5f479b76e10fae2a7ae3c64cfb7a6f181e
Appaz <matias2007play4@gmail.com>
2023-10-20 17:14:35 -0300

    Buen comienzo

--- TP Nro. 4/Consultas.sql
+++ TP Nro. 4/Consultas.sql
@@ -1,9 +1,19 @@
-select paquetes_turisticos.codigo, paquetes_turisticos.destino, paquetes_turisticos.precio, agencias.nombre from paquetes_turisticos, agencias where agencias.codigo = paquetes_turisticos.agencia;
+1- select paquetes_turisticos.codigo, paquetes_turisticos.destino, paquetes_turisticos.precio, agencias.nombre from paquetes_turisticos, agencias where agencias.codigo = paquetes_turisticos.agencia;
 
-select clientes.DNI, clientes.nombre, clientes.domicilio from clientes, paquetes_turisticos where clientes.DNI = paquetes_turisticos.cliente;
+2- select clientes.DNI, clientes.nombre, clientes.domicilio from clientes, paquetes_turisticos where clientes.DNI = paquetes_turisticos.cliente;
 
-select clientes.nombre from clientes where nombre like 'P%';
+3-
 
-select codigo, precio from paquetes_turisticos where precio > 400000 order by precio;
+4-
 
-select nombre, ciudad from agencias where ciudad = 'CABA';
+5- select clientes.nombre from clientes where nombre like 'P%';
+
+6- select codigo, precio from paquetes_turisticos where precio > 400000 order by precio;
+
+7- 
+
+8- 
+
+9-
+
+10- select nombre, ciudad from agencias where ciudad = 'CABA';
