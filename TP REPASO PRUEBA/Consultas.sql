276580dbd34f66886cc3ea71b94a692bdcfa2e41
Appaz <matias2007play4@gmail.com>
2023-10-27 14:28:34 -0300

    Correccion

--- TP REPASO PRUEBA/Consultas.sql
+++ TP REPASO PRUEBA/Consultas.sql
@@ -9,3 +9,9 @@ select Nombre as "Piezas que terminan con 's'" from Piezas where Nombre like "%s
 select Nombre from Proveedores where Proveedores.ID in (select IdProveedor from Suministra where CodigoPieza = 1);
 
 select Nombre from Piezas where Piezas.Codigo in (select CodigoPieza from Suministra where IdProveedor = 'HAL');
+
+select Piezas.Nombre, Suministra.Precio from Piezas, Suministra where Piezas.Codigo = Suministra.CodigoPieza and Suministra.Precio in (select max(Precio) from Suministra);
+
+
+
+update Suministra set Precio = Precio + 100;
