1d81365a3a1dfdc673405ec0925cd823ecccc107
Appaz <matias2007play4@gmail.com>
2023-10-27 17:21:34 -0300

    Correcciones

--- TP Grupo 1/Consultas.sql
+++ TP Grupo 1/Consultas.sql
@@ -5,3 +5,6 @@ select telefono, ciudad from oficina where pais like "Esp%";
 select email, nombre, apellido1, apellido2 from empleado where codigo_jefe = 7;
 
 select puesto, nombre, apellido1, apellido2, email from empleado where codigo_jefe is NULL;
+
+select nombre, apellido1, apellido2, puesto from empleado where puesto != 'Representante Ventas';
+
