7b7a92d33d023cf92f2c220fc731d2d25369a7f1
Appaz <matias2007play4@gmail.com>
2023-10-27 16:08:15 -0300

    Correcciones

--- TP REPASO PRUEBA/Consultas.sql
+++ TP REPASO PRUEBA/Consultas.sql
@@ -1,22 +1,22 @@
-select Nombre as "Piezas" from Piezas;
+a- select Nombre as "Piezas" from Piezas;
 
-select ID as "ID del proveedor", Nombre as "Nombre del proveedor" from proveedores;
+b- select ID as "ID del proveedor", Nombre as "Nombre del proveedor" from proveedores;
 
-select avg(Precio) as "Precio medio" from Suministra;
+c- select avg(Precio) as "Precio medio" from Suministra;
 
-select Nombre as "Piezas que terminan con 's'" from Piezas where Nombre like "%s";
+d- select Nombre as "Piezas que terminan con 's'" from Piezas where Nombre like "%s";
 
-select Nombre from Proveedores where Proveedores.ID in (select IdProveedor from Suministra where CodigoPieza = 1);
+e- select Nombre from Proveedores where Proveedores.ID in (select IdProveedor from Suministra where CodigoPieza = 1);
 
-select Nombre from Piezas where Piezas.Codigo in (select CodigoPieza from Suministra where IdProveedor = 'HAL');
+f- select Nombre from Piezas where Piezas.Codigo in (select CodigoPieza from Suministra where IdProveedor = 'HAL');
 
-select Piezas.Nombre, Suministra.Precio from Piezas, Suministra where Piezas.Codigo = Suministra.CodigoPieza and Suministra.Precio in (select max(Precio) from Suministra);
+g- select Piezas.Nombre, Suministra.Precio from Piezas, Suministra where Piezas.Codigo = Suministra.CodigoPieza and Suministra.Precio in (select max(Precio) from Suministra);
 
-insert into Suministra(CodigoPieza, IdProveedor, Precio)
-values(11, 'TNBC', 10);
+h- insert into Suministra(CodigoPieza, IdProveedor, Precio)
+   values(11, 'TNBC', 10);
 
-update Suministra set Precio = Precio + 100;
+i- update Suministra set Precio = Precio + 100;
 
-delete from Suministra where IdProveedor = "RBT";
+j- delete from Suministra where IdProveedor = "RBT";
 
-delete from Suministra where IdProveedor = "RBT" and CodigoPieza in (select Codigo from Piezas where Nombre = "Clavos");
+k- delete from Suministra where IdProveedor = "RBT" and CodigoPieza in (select Codigo from Piezas where Nombre = "Clavos");
