03f0058a03342b5fed187c1c5d76a9d68fe3be72
Appaz <matias2007play4@gmail.com>
2023-10-27 17:11:06 -0300

    Correcciones

---
+++ TP Grupo 1/Consultas.sql
@@ -0,0 +1,7 @@
+select codigo_oficina, ciudad from oficina;
+
+select telefono, ciudad from oficina where pais like "Esp%";
+
+select email, nombre, apellido1, apellido2 from empleado where codigo_jefe = 7;
+
+
