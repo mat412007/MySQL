fd5fd91c8457ecdb56aff7fd388e7e8cb7575636
Appaz <matias2007play4@gmail.com>
2023-11-01 11:41:19 -0300

    Progreso

--- TP Grupo 1/Consultas.sql
+++ TP Grupo 1/Consultas.sql
@@ -15,3 +15,13 @@ select distinct estado from pedido;
 select cliente.codigo_cliente from cliente inner join pago on cliente.codigo_cliente = pago.codigo_cliente where year(pago.fecha_pago) = 2008;
 
 
+
+
+
+select * from pedido where estado = 'Rechazado' and year(fecha_entrega) = 2009;
+
+select * from pedido where month(fecha_entrega) = 01 and estado = 'Entregado';
+
+select * from pago where year(fecha_pago) = 2008 and forma_pago = 'Paypal';
+
+select * from pago where year(fecha_pago) = 2008 and forma_pago = 'Paypal';
