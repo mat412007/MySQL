13f452b843dd1e1edd34bbd9c5fb66a91dd5a35a
Appaz <matias2007play4@gmail.com>
2023-11-06 10:54:17 -0300

    Correccion

---
+++ TP Nro. 4/Insert.sql
@@ -0,0 +1,21 @@
+insert into Agencias(Codigo, Fecha_Inicial, Ciudad)
+values(11111, '2000-12-24', 'Madrid'),
+(22222, '1980-05-15', 'Paris'),
+(33333, '2010-04-09', 'Nueva York'),
+(44444, '2005-08-02', 'Roma'),
+(55555, '2020-01-01', 'Londres'),
+(66666, '1950-10-28', 'Barcelona'),
+(77777, '2015-01-30', 'Washington'),
+(88888, '1990-07-20', 'CABA'),
+(99999, '1970-06-02', 'Tokyo');
+
+insert into Clientes(DNI, Domicilio, Nombre, Apellido)
+values(47589631, 'Nogoya 2343', 'Juan', 'Lopez'),
+(12463589, 'Argerich 3562', 'Max', 'Bayers'),
+(16478532, 'Nazca 1645', 'Tomas', 'Guevara'),
+(46214695, 'Arregui 2846', 'Francisco', 'Donia'),
+(16794652, 'Av. Lope de Vega 1298', 'Pedro', 'Longo'),
+(34899665, 'Av. San Martin 2467', 'Eduardo', 'Grin'),
+(24683157, 'Santo Tome 3758', 'Bauti', 'Mambrin'),
+(31648289, 'Argerich 3850', 'Marco', 'Sanzchez');
+
