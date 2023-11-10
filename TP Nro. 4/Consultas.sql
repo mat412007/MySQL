083a0fda9e5e72cc1e38cece27cb21d72346fcf7
Appaz <matias2007play4@gmail.com>
2023-11-10 14:35:52 -0300

    Avance

--- TP Nro. 4/Consultas.sql
+++ TP Nro. 4/Consultas.sql
@@ -7,7 +7,7 @@ select dni, nombre, Apellido from clientes, Paquetes_Clientes, paquetes where cl
 3-
 
 4-
-select Formas_de_Pago.banco, clientes.nombre from Formas_de_Pago, clientes where clientes.Forma_de_pago = Formas_de_Pago.Tipo order by formas_de_pago.banco;
+select banco,Cliente_DNI from Formas_Pago, clientes where clientes.DNI = Formas_Pago.Cliente_DNI;
 
 5- 
 select clientes.nombre from clientes where nombre like 'P%';
@@ -16,8 +16,7 @@ select clientes.nombre from clientes where nombre like 'P%';
 select codigo, precio from paquetes where precio > 400000 order by precio;
 
 7- 
-update Paquetes set Precio = (Precio * 0.7) where (select clientes.DNI from clientes) = (select Paquetes_Clientes.DNI_Cliente from Paquetes_Clientes);
-select clientes.DNI, paquetes.precio from clientes, paquetes, Paquetes_Clientes where clientes.DNI = Paquetes_Clientes.DNI_Cliente order by clientes.dni;
+update Paquetes set Precio = (Precio * 0.9) where paquetes.codigo in(select Codigo_Paquete from Paquetes_Clientes);
 
 8- 
 
