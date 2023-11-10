01b0aa49e7b33c11d4c227471118fad16c759ddc
Appaz <matias2007play4@gmail.com>
2023-11-10 14:22:50 -0300

    Avance

--- TP Nro. 4/Consultas.sql
+++ TP Nro. 4/Consultas.sql
@@ -1,8 +1,8 @@
 1- 
-select paquetes_turisticos.codigo, paquetes_turisticos.destino, paquetes_turisticos.precio, agencias.nombre from paquetes_turisticos, agencias where agencias.codigo = paquetes_turisticos.agencia;
+select destino, codigo, precio, agencia from paquetes;
 
 2- 
-select clientes.DNI, clientes.nombre, clientes.domicilio from clientes, paquetes_turisticos where clientes.DNI = paquetes_turisticos.cliente;
+select dni, nombre, Apellido from clientes, Paquetes_Clientes, paquetes where clientes.DNI = Paquetes_Clientes.DNI_Cliente and paquetes.Codigo = Paquetes_Clientes.Codigo_Paquete;
 
 3-
 
@@ -13,15 +13,15 @@ select Formas_de_Pago.banco, clientes.nombre from Formas_de_Pago, clientes where
 select clientes.nombre from clientes where nombre like 'P%';
 
 6- 
-select codigo, precio from paquetes_turisticos where precio > 400000 order by precio;
+select codigo, precio from paquetes where precio > 400000 order by precio;
 
 7- 
-update Paquetes_Turisticos set Precio = (Precio * 0.7) where (select clientes.DNI from clientes) = (select paquetes_turisticos.cliente from paquetes_turisticos);
-select clientes.DNI, paquetes_turisticos.precio from clientes, paquetes_turisticos where clientes.DNI = paquetes_turisticos.cliente;
+update Paquetes set Precio = (Precio * 0.7) where (select clientes.DNI from clientes) = (select Paquetes_Clientes.DNI_Cliente from Paquetes_Clientes);
+select clientes.DNI, paquetes.precio from clientes, paquetes, Paquetes_Clientes where clientes.DNI = Paquetes_Clientes.DNI_Cliente order by clientes.dni;
 
 8- 
 
 9-
 
 10- 
-select nombre, ciudad from agencias where ciudad = 'CABA';
+select * from agencias where ciudad = 'CABA';
